4dda13a8d6f5a5192d554c7ac0676d9cb46a559b
Luc Somers <lucsomers@hotmail.com>
2023-02-20 12:39:26 +0100

    Added new class for naming references

--- ExternalReferenceFinder/ExternalReferenceFinder/Program.cs
+++ ExternalReferenceFinder/ExternalReferenceFinder/Program.cs
@@ -1,47 +1,55 @@
-﻿using Microsoft.Build.Construction;
-using System.Reflection;
+﻿using ExternalReferenceFinder;
+using Microsoft.Build.Construction;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
+        FindAllSolutionsBase();
+    }
+
+    private static void FindAllSolutionsBase()
+    {
         //find all solutions in folder
         string path = Console.ReadLine();
 
-        List<SolutionFile> solutionFiles = FindAllSolutions(path);
+        List<SolutionFileData> solutionFiles = FindAllSolutions(path);
+
+
 
         //find all projects in solution
-        foreach(SolutionFile solutionFile in solutionFiles)
+        foreach (SolutionFileData solutionFile in solutionFiles)
         {
             Console.WriteLine("------------------");
 
-            Console.WriteLine($"Found solution {solutionFile}. Projects in this solution:");
+            Console.WriteLine($"Found solution {solutionFile.GetSolutionName()} Projects in this solution:");
 
-            foreach (ProjectInSolution project in solutionFile.ProjectsInOrder)
+            foreach (ProjectInSolution project in solutionFile.SolutionFile.ProjectsInOrder)
             {
                 //find all methods in project
+                //if (project.ProjectName != "Contoso.Common")
+                //    continue;
                 Console.WriteLine($"       {project.ProjectName}");
             }
 
             Console.WriteLine("------------------");
         }
-        //check if they are a reference to the given method
     }
 
-    public static List<SolutionFile> FindAllSolutions(string directoryPath)
+    public static List<SolutionFileData> FindAllSolutions(string directoryPath)
     {
-        List<SolutionFile> solutionFiles = new List<SolutionFile>();
+        List<SolutionFileData> solutionFiles = new List<SolutionFileData>();
 
         string[] solutionPaths = Directory.GetFiles(directoryPath, "*.sln", SearchOption.AllDirectories);
 
         foreach (string solutionPath in solutionPaths)
-        { 
+        {
             SolutionFile solutionFile = SolutionFile.Parse(solutionPath);
 
             if (solutionFile == null)
                 continue;
 
-            solutionFiles.Add(solutionFile);
+            solutionFiles.Add(new SolutionFileData(solutionFile, solutionPath));
         }
 
         return solutionFiles;
---
+++ ExternalReferenceFinder/ExternalReferenceFinder/SolutionFileData.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Build.Construction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExternalReferenceFinder
+{
+    internal class SolutionFileData
+    {
+        private SolutionFile solutionFile;
+        private string solutionName;
+        private string solutionPath;
+
+        public SolutionFileData(SolutionFile solutionFile, string solutionPath)
+        {
+            this.solutionFile = solutionFile;
+            this.solutionPath = solutionPath;
+            this.solutionName = "";
+        }
+
+        public string GetSolutionName()
+        {
+            if (solutionName != "")
+                return solutionName;
+
+            string[] split = solutionPath.Split('\\');
+
+            solutionName = split[^1];
+
+            return solutionName;
+        }
+
+        public SolutionFile SolutionFile { get => solutionFile; set => solutionFile = value; }
+        public string SolutionPath { get => solutionPath; set => solutionPath = value; }
+    }
+}
